b9d0b6fd37d9a608b4ca2cadc303910f4845ee69
Colby Beach <38380295+clawplusstacker@users.noreply.github.com>
2021-10-25 15:56:37 -0400

    Update software.js
    Made software page more dynamic and more cross compatible.

--- software.js
+++ software.js
@@ -1,67 +1,58 @@
+function originalCSS(){
+  $("#bcHalfLeft").css("float", "left");
+  $("#bcHalfLeft").css("height", "1100px");
+
+  $("#bcHalfRight").css("float", "right");
+  $("#bcHalfRight").css("height", "100%");
+  $("#bcHalfRight").css("width", "50%");
+
+
+  $(".grid-container").css("width", "67%")
+  $(".grid-item").css("font-size", "120%")
+}
+
+function changeCSS(){
+
+
+  $("#bcHalfLeft").css("float", "none");
+  $("#bcHalfLeft").css("margin", "auto");
+  $("#bcHalfLeft").css("height", "500px");
+
+  $("#bcHalfRight").css("float", "none");
+  $("#bcHalfRight").css("margin", "auto");
+  $("#bcHalfRight").css("height", "1000px");
+  $("#bcHalfRight").css("width", "100%");
+
+
+  $(".grid-container").css("width", "100%")
+  $(".grid-item").css("font-size", "100%")
+
+}
+
 $(function() {
 
-    
     var win = $(this);
 
     $(window).on('resize', function(){
 
       if (win.width() > 750) {
 
-          $("#bcHalfLeft").css("float", "left");
-          $("#bcHalfLeft").css("height", "1100px");
-
-          $("#bcHalfRight").css("float", "right");
-
-
-          $(".grid-container").css("width", "67%")
-          $(".grid-item").css("font-size", "120%")
+        originalCSS()
 
       }else{
     
-
-        //Software Page 
-        $("#bcHalfLeft").css("float", "none");
-        $("#bcHalfLeft").css("margin", "auto");
-        $("#bcHalfLeft").css("height", "500px");
-
-        $("#bcHalfRight").css("float", "none");
-        $("#bcHalfRight").css("margin", "auto");
-
-
-        $(".grid-container").css("width", "100%")
-        $(".grid-item").css("font-size", "100%")
-
+        changeCSS()
       }
 
     });
 
-
      $(document).ready(function() {
-        if (win.width() > 750) {
+        if (!win.width() > 750) {
        
-
-            $("#bcHalfLeft").css("float", "left");
-            $("#bcHalfLeft").css("height", "1100px");
-            $("#bcHalfRight").css("float", "right");
-
-            $(".grid-container").css("width", "67%")
-            $(".grid-item").css("font-size", "120%")
-
+          originalCSS()
 
         }else{
-
-            $("#bcHalfLeft").css("float", "none");
-            $("#bcHalfLeft").css("margin", "auto");
-            $("#bcHalfLeft").css("height", "500px");
-
-
-            $("#bcHalfRight").css("float", "none");
-            $("#bcHalfRight").css("margin", "auto");
-
-            $(".grid-container").css("width", "100%")
-            $(".grid-item").css("font-size", "100%")
-
-
+          changeCSS()
         }
   });
 
