252a0b380743c0ff6d89dcf845f98fb6e90ab97e
Colby Beach <clawplusstacker@gmail.com>
2021-12-13 15:46:09 -0500

    Trying to optimize website for mobile

--- home.js
+++ home.js
@@ -1,37 +1,41 @@
 function originalCSS(){
-    $("#homeBody1").css("height", "600px");
-    $("#body1Right").css("float", "right");
+
+  $("#homeBody1").css("height", "600px");
+  $("#homeBody1").css("background-color", "black");
+
 
 }
 
+
 function changeCSS(){
-    $("#homeBody1").css("height", "30000px");
-    $("#body1Right").css("float", "none");
+  $("#homeBody1").css("height", "1000px");
+  $("#homeBody1").css("background-color", "red");
+
 
 }
 
 $(function() {
 
-    var win = $(this);
+  var win = $(this);
 
-    //When resizing 
-    $(window).on('resize', function(){
+  //When resizing 
+  $(window).on('resize', function(){
+
+    if (win.width() > 750) {
+      originalCSS()
+    }else{
+      changeCSS()
+    }
+
+  })
+
+  //On load 
+   $(document).ready(function() {
 
       if (win.width() > 750) {
         originalCSS()
       }else{
         changeCSS()
       }
-
-    });
-
-    //On load 
-     $(document).ready(function() {
-
-        if (win.width() > 750) {
-          originalCSS()
-        }else{
-          changeCSS()
-        }
-  });
-});
+  })
+})
