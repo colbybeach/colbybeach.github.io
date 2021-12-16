c87f1671ff1c366fe3525cc28a68455c8c15b3d1
Colby Beach <38380295+clawplusstacker@users.noreply.github.com>
2021-12-15 20:11:16 -0500

    Working on nav bar bugs

--- navbar.js
+++ navbar.js
@@ -1,64 +1,57 @@
 $(function() {
 
-      var header = $(".navBarNew");
-      var downarrow = $(".downarrowImage");
-      var readmore = $(".readmore");
+  var win = $(this);
+  var header = $(".navBarNew");
 
-      
+
+  $(window).on('resize', function(){
+
+    if (win.width() > 750) {
+      scrollChange();
+      header.removeClass("navBarScroll")
+    }else{
+      header.addClass("navBarScroll")
+    }
+
+  });
+
+  $(document).ready(function() {
+  
+    if (win.width() > 750) {
+      scrollChange();
+    }else{
+      header.addClass("navBarScroll")
+    }
+
+  });  
+
+})
+
+function scrollChange() {
+  var header = $(".navBarNew");
+  var readmore = $(".readmore");
+
+  
+  var scroll = $(window).scrollTop();
+
+
+
+
+  $(window).scroll(function() {
+
       var scroll = $(window).scrollTop();
-      var win = $(this);
 
+      if (scroll >= 100) {
+          header.addClass("navBarScroll");
+      } else {
+          header.removeClass("navBarScroll");
+      }
 
+      if(scroll >= 150){
+          readmore.addClass("scrollFade");
 
-
-      $(window).scroll(function() {
-
-          var scroll = $(window).scrollTop();
-
-          if (scroll >= 100) {
-              header.addClass("navBarScroll");
-          } else {
-              header.removeClass("navBarScroll");
-          }
-
-          if(scroll >= 150){
-              readmore.addClass("scrollFade");
-
-          }else {
-            readmore.removeClass("scrollFade");
-          }
-
-
-        });
-
-      $(window).on('resize', function(){
-
-        if (win.width() > 750) {
-
-            $("#body1Left").css("font-size", "18px");
-            $("#halfHalf1 img").css("width","430px");
-
-            //Software Page 
-            $("#bcHalfLeft").css("float", "left");
-            $("#bcHalfRight").css("float", "right");
-            $("#bcHalfRight").css("float", "right");
-
-        }else{
-          $("#body1Left").css("font-size", "1.3vw");
-          $("#halfHalf1 img").css("width","70%");
-
-
-        }
-
-
-      });
-
-       $(document).ready(function() {
-          if (win.width() > 750) {
-            $("#body1Left").css("font-size", "18px");
-            $("#halfHalf1 img").css("width","430px");
-
-          }
+      }else {
+        readmore.removeClass("scrollFade");
+      }
     });
-
-});
+}
